9d224631a23a305903fdbf8763b699d93b8b033d
MisaelSantana <misaelsantana2011hotmail.com>
2021-08-19 22:01:36 -0300

    submit modify

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -11,22 +11,32 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-
-  <ion-card>
-    <ion-card-header>
-      <ion-card-title>Login</ion-card-title>
-    </ion-card-header>
-    <!-- Input with placeholder -->
-    <ion-input type="text" placeholder="Input Your Username"></ion-input>
-    <ion-input type="password" placeholder="Input Your Password"></ion-input>
-    <ion-button color="light">Register</ion-button>
-    <ion-button color="dark">Login</ion-button>
-    
-  </ion-card>
+  <div id="container">
+    <!-- Header Card -->
+    <ion-card>
+      <ion-card-header>
+        <ion-card-title>Login</ion-card-title>
+      </ion-card-header>
+      <!-- Input with placeholder -->
+      <ion-input type="text" placeholder="Input Your Username"></ion-input>
+      <ion-input type="password" placeholder="Input Your Password"></ion-input>
+      <ion-button color="light">Register</ion-button>
+      <ion-button color="dark">Login</ion-button>
+    </ion-card>
+  </div>
       <ExploreContainer name="Tab 1 page" />
     </ion-content>
   </ion-page>
 </template>
+
+<style scoped>
+#container {
+  display: flex;
+  justify-content: center;
+  max-width: 100%;
+  align-items: center;
+}
+</style>
 
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
